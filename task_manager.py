72da9653a95b064f5efb2fd1d1db04431aeb4ab5
Guga <gugakakashvili1337@gmail.com>
2026-01-16 17:16:22 +0400

    initial commit

---
+++ task_manager.py
@@ -0,0 +1,86 @@
+import json
+import os
+TASKS_FILE = "tasks.json"
+
+def load_tasks():
+    if not os.path.exists(TASKS_FILE):
+        return []
+    
+    with open(TASKS_FILE, "r") as file:
+        return json.load(file)
+
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks,file,indent=4)
+
+
+
+class Task():
+    def __init__(self,title,complete = False):
+        self.title = title
+        self.complete = complete
+    
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "complete": self.complete
+        }
+    
+
+class TaskManager():
+    def __init__(self):
+        self.tasks = load_tasks()
+
+    def add_task(self,title):
+        self.tasks = load_tasks()
+        task = Task(title)
+        self.tasks.append(task.to_dict())
+
+        save_tasks(self.tasks)
+
+    def delete_task(self,index):
+        self.tasks = load_tasks()
+        self.tasks.pop(index - 1)
+        save_tasks(self.tasks)
+    
+    def complete_task(self,index):
+        self.tasks = load_tasks()
+        self.tasks[index - 1]["complete"] = True
+        save_tasks(self.tasks)
+    
+    def list_tasks(self):
+        self.tasks = load_tasks()
+
+        if not self.tasks:
+            print("No tasks found")
+            return
+        
+        for i, task in enumerate(self.tasks, start=1):
+            status = "✓" if task["complete"] else " "
+            print(f"[{i}] [{status}] {task['title']}")
+
+
+manager = TaskManager()
+
+while True:
+
+    command = input("add/delete/complete/list/quit ").strip().lower()
+
+    if command == "add":
+        title = input("input title: ")
+        manager.add_task(title)
+    elif command == "delete":
+        
+        index = int(input("input number: "))
+        manager.delete_task(index)
+    elif command == "complete":
+        index = int(input("input number: "))
+        manager.complete_task(index)
+    elif command == "list":
+        if not manager.tasks:
+            print("List is empty")
+        manager.list_tasks()
+    elif command == "quit":
+        break
+    else:
+        print("Unknown command")
